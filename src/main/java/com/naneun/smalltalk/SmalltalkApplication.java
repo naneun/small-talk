323fe0cbb1854afc3cd180eab90f36efb846c63c
naneun <rmtlrksdp@naver.com>
2022-10-14 14:54:12 +0900

    🎉 small-talk 프로젝트 시작

---
+++ src/main/java/com/naneun/smalltalk/SmalltalkApplication.java
@@ -0,0 +1,12 @@
+package com.naneun.smalltalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmalltalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmalltalkApplication.class, args);
+	}
+}
