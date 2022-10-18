147d20a97e3cd894e8f5990c517228de009ff682
naneun <rmtlrksdp@naver.com>
2022-10-18 19:19:27 +0900

    ✨ 테스트 코드 작성을 위한 간이 채팅방 생성 기능 추가

---
+++ src/main/java/com/naneun/smalltalk/chat/api/ChatRoomRequest.java
@@ -0,0 +1,21 @@
+package com.naneun.smalltalk.chat.api;
+
+import com.naneun.smalltalk.chat.ChatRoom;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class ChatRoomRequest {
+
+    private String title;
+
+    public ChatRoom toEntity() {
+        return ChatRoom.builder()
+                .id(1L)
+                .title(title)
+                .build();
+    }
+}
